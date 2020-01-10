3ced4cfd20fa4e15d962f28d5803c7fb6e941104
Ikha Wahyu Ramadhan <ikhawahyuramadhan@gmail.com>
2020-01-10 22:07:18 +0700

    Add files via upload

---
+++ src/main/java/com/bandingin/wahyu/WahyuApplication.java
@@ -0,0 +1,13 @@
+package com.bandingin.wahyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WahyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WahyuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandingin/wahyu/WahyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandingin.wahyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WahyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
